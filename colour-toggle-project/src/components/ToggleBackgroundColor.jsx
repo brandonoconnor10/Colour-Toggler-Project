32e55cd9bc8a5cea82edbb1bfa6830ff371aa49e
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-23 17:30:58 +0200

    Update ToggleBackgroundColor.jsx

--- colour-toggle-project/src/components/ToggleBackgroundColor.jsx
+++ colour-toggle-project/src/components/ToggleBackgroundColor.jsx
@@ -8,7 +8,7 @@ function ToggleBackgroundColor() {
 
   function handleClick() {
     setBackgroundColor(backgroundColor === "white" ? "#1b1b1b" : "white");
-    setTextColor(textColor === "#1b1b1b" ? "#ffa31a" : "#1b1b1b");
+    setTextColor(textColor === "#1b1b1b" ? "crimson" : "#1b1b1b");
     setButtonStyle(backgroundColor === "white" ? "#1b1b1b" : "white");
   }
 
